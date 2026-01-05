4ea9ca15be1b42da4b1f4626b05181f0b7debb91
xicelestin <selemusic1@gmail.com>
2026-01-05 15:38:59 -0800

    chore: Add export utilities to utils package exports
    Export all new export utility functions from utils package:
    - create_download_button, create_multi_format_export
    - create_filtered_export_section, export_with_metadata
    - to_csv, to_excel, to_json
    
    Makes export functionality easily accessible throughout the dashboard.

--- dashboard/utils/__init__.py
+++ dashboard/utils/__init__.py
@@ -10,6 +10,16 @@ from .data_validation import (
     validate_dashboard_data,
     get_validated_data,
     safe_divide
+
+from .export_utils import (
+    create_download_button,
+    create_multi_format_export,
+    create_filtered_export_section,
+    export_with_metadata,
+    to_csv,
+    to_excel,
+    to_json
+)
 )
 
 __all__ = [
@@ -17,6 +27,14 @@ __all__ = [
     'validate_dashboard_data',
     'get_validated_data',
     'safe_divide'
+        # Export utilities
+    'create_download_button',
+    'create_multi_format_export',
+    'create_filtered_export_section',
+    'export_with_metadata',
+    'to_csv',
+    'to_excel',
+    'to_json',
 ]
 
 __version__ = '1.0.0'
