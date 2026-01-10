6ff352b0eea94d084035b3fda3c3097673e70768
xicelestin <selemusic1@gmail.com>
2026-01-09 22:22:32 -0800

    Fix: Add proper indentation to time period filtering section (lines 351-358)

--- dashboard/app.py
+++ dashboard/app.py
@@ -348,14 +348,14 @@ with col_filter1:
         )
     
     # Filter data based on selection
-    if time_period == "Last 7 Days":
-        data_filtered = data.tail(7)
-    elif time_period == "Last 30 Days":
-        data_filtered = data.tail(30)
-    elif time_period == "Last 90 Days":
-        data_filtered = data.tail(90)
-    else:
-        data_filtered = data
+        if time_period == "Last 7 Days":
+                data_filtered = data.tail(7)
+        elif time_period == "Last 30 Days":
+                data_filtered = data.tail(30)
+        elif time_period == "Last 90 Days":
+                data_filtered = data.tail(90)
+        else:
+                data_filtered = data
     
     # Recalculate KPIs for filtered period
     kpis = calculate_kpis(data_filtered)
