294d688c266d8411df2cc2805ac4b988095b7e0a
xicelestin <selemusic1@gmail.com>
2026-02-05 03:47:29 -0800

    Fix syntax error: remove extra closing parenthesis on line 321

--- dashboard/data_connector.py
+++ dashboard/data_connector.py
@@ -318,7 +318,7 @@ class DataConnector:
                     'cohort': cohort,
                     'period': period,
                     'week': i,
-                    'retention': max(0.5, 1.0 - (i * 0.08)))
+                    'retention': max(0.5, 1.0 - (i * 0.08))
                             })
         
         return {
